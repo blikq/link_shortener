ce053b9df032280c6e5d28689e7cd086d9275f69
Blikq <blikqmain@gmail.com>
2022-10-26 11:00:40 +0100

    development ready

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from .models import m_link, r_link
 # Create your views here.
 
 def links_srt(request):
-
+    
     ini_ = request.headers["link"]
     srt = m_link(ini_)
     return JsonResponse({"success": True, "link": srt})
@@ -13,8 +13,12 @@ def links_srt(request):
 def redirect_srt(request, link):
     l = r_link(link)
     if l:
-        url = f'https://google.com/?q={l}'
-        return redirect(url)
+        if l[0:8] == 'https://':
+            return redirect(l)
+        elif l[0:7] == 'http://':
+            return redirect(l)
+        else:
+            return redirect(f'https://google.com/?q={l}')
     else:
         return HttpResponse('Sorry but the link is invalid. Generate a link >>> <a href="http://localhost:8000" style=color:"red";>Here</a>')
 
